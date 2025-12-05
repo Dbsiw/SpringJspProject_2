c2bc22cafc84c382f1745355f245fbe8858a0426
JUN <worldjs12@naver.com>
2025-12-05 17:08:32 +0900

    1234

---
+++ src/test/java/com/sist/web/SpringJspProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sist.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJspProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
